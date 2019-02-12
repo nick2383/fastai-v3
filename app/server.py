2a6b51eb86591b61fcc95ba9f805c7c527a72afd
Nicholas Birch <nicholasbirch@Nicholass-MacBook-Pro.local>
2019-02-11 18:36:01 -0800

    first commit

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/a/iotatel.com/uc?authuser=0&id=1QKIAeZnqosfR4UZTh5Gmojl5YCpxLgiz&export=download'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
